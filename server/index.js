3ff416af34415f1396a67151d0cd262d4c4dbe5d
getxiaofeng <l>
2019-07-13 21:04:32 +0800

    生产环境编译

--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ app.set('secret','adefdvdvffgaxsa') //在app实例上设置一个secret变量，
 
 app.use(require('cors')())  //中间件，解决跨域问题  
 app.use(express.json())     //中间件，解析json数据格式
-app.use('/uploads',express.static(__dirname+'/uploads'))
+app.use('/',express.static(__dirname+'/web'))           //托管web页面文件
+app.use('/admin/',express.static(__dirname+'/admin'))    //后后台管理界面文件
+app.use('/uploads',express.static(__dirname+'/uploads'))     // 上传文件
 
 require('./router/admin')(app)      //这里接收admin下index.js中的app并使用
 require('./plugins/db')(app) 
